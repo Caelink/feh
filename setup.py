1a1aa9f26fd041e1f7e334f5fea708bd9dddb203
Caelin Jackson-King <caelink@gmail.com>
2017-12-18 13:02:41 -0800

    setuptools, fixed board_test maybe

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
-#!/usr/bin/env python
+from setuptools import setup
+from setuptools import find_packages
 
-from distutils.core import setup
-
-setup(name='feh',
-      version='0.0',
-      description='Fire Emblem Heroes Python Board module',
-      author='Caelin Jackson-King',
-      author_email='caelink+feh@gmail.com',
-      url='https://github.com/Caelink/feh',
-      packages=['feh'],
-     )
+setup(
+    name='feh',
+    version='0.0',
+    description='Fire Emblem Heroes Python Board module',
+    author='Caelin Jackson-King',
+    author_email='caelink+feh@gmail.com',
+    url='https://github.com/Caelink/feh',
+    packages=find_packages(exclude=('tests*',)),
+    install_requires=[
+        'six',
+    ],
+)
--- tests/board_test.py
+++ tests/board_test.py
@@ -1,15 +1,17 @@
 from feh.board import normal
+from feh.board import normal_defensive
+from feh.board import tree
+from feh.board import tree_defensive
+from feh.board import flyable
+from feh.board import impassable
 
 
 def test_defensive_tiles():
-    assert not is_defensive(Space.normal)
-    assert is_defensive(Space.normal_defensive)
-    assert not is_defensive(Space.tree)
-    assert is_defensive(Space.tree_defensive)
-    assert not is_defensive(Space.impassable)
+    assert normal().is_defensive
+    assert not normal_defensive().is_defensive
+    assert tree().is_defensive
+    assert not tree_defensive().is_defensive
+    assert flyable().is_defensive
+    assert not impassable().is_defensive
 
-
-try:
-    test_defensive_tiles()
-except AssertionError as e:
-    print "Failure", e
+test_defensive_tiles()
